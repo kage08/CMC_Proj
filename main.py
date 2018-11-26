131071528c9b832f3dba337c5e1727469823d2d3
kage08 <harshavardhan864.hk@gmail.com>
2018-11-26 13:49:41 +0530

    Pushed Regressor GA

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(20,20))
 
-gr = pointGraph(n=20)
+gr = pointGraph(n=40)
 #ga = TSPSolver(graph=gr,cut_frac=1.0,initial_popsize=500, selection_fun=minmax)
-#ga.train(iters=500, plot=True, plotresult=True
-#)
-penalty = np.ones(20)*0.2
+#ga.train(iters=500, plot=True, plotresult=True)
+
+penalty = np.ones(40)*10
 tsp_params={}
-ma = MetaSolver(graph=gr, penalty=penalty,subiters=50,sub_pop_size=500,pop_size=10,
+ma = MetaSolver(graph=gr, penalty=penalty,subiters=50,sub_pop_size=2000,pop_size=10,
     selection_fun=percentile,
-    fitness = lambda x:np.exp(-x), crossover_rate=0.5, min_frac=0.2, mutation_rate=0.2, tsp_params=tsp_params)
+    fitness = lambda x:np.exp(-x), crossover_rate=0.5, min_frac=0.7, mutation_rate=0.2, tsp_params=tsp_params)
